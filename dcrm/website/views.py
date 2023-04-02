350be75b57f2db4dd1d05224d4790bce04200ab6
ObedRav <obedrayo2356@gmail.com>
2023-04-02 20:26:56 +0000

    Adding the login functionality

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html', {})
+def home(request) -> render.__class__:
+    # Ckeck if logging in
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful')
+        else:
+            messages.error(request, 'Login failed')
+
+        return redirect('home')
+
+    else:
+        return render(request, 'home.html', {})
+
+
+def logout_user(request) -> redirect.__class__:
+    logout(request)
+    messages.success(request, 'You have been logged out')
+    print(request.__class__.__name__)
+    return redirect('home')
